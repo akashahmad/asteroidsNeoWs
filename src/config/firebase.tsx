c4023dce31a852fb6d4897ccba8d77e5c19fb650
akashahmad <engr.akashahmad@gmail.com>
2020-10-17 05:23:01 +0500

    firebase configration added.

---
+++ src/config/firebase.tsx
@@ -0,0 +1,11 @@
+import * as firebase from "firebase";
+
+export default firebase.initializeApp({
+    apiKey: "AIzaSyAUngXx3iL6jwdjJKZWaWfR8PQd9qGkMWg",
+    authDomain: "newws-e4b3a.firebaseapp.com",
+    databaseURL: "https://newws-e4b3a.firebaseio.com",
+    projectId: "newws-e4b3a",
+    storageBucket: "newws-e4b3a.appspot.com",
+    messagingSenderId: "15167388098",
+    appId: "1:15167388098:web:eb1e7180709e9f6c8c3e5b"
+});
--- src/pages/signUpPage/signUppage.tsx
+++ src/pages/signUpPage/signUppage.tsx
@@ -1,6 +1,30 @@
 import React from 'react'
 import  SignUpTemplate from '../../templates/signUpTemplate/signUpTemplate'
+// import firebase from '../../config/firebase'
+// import {useEffect} from "react";
 const SignUpPage = () => {
+    // useEffect(() => {
+    //     firebase.auth().createUserWithEmailAndPassword("email@test.com", "12345678").then((data:any) => {
+    //         console.log("auth created successfully", data.user);
+    //         const uid = data.user.uid;
+    //         const userObj = {
+    //             uid,
+    //             firstName: "John",
+    //             lastName: "Doe",
+    //             email: "email@test.com"
+    //         };
+    //         firebase.firestore().collection('users')
+    //             .doc(uid)
+    //             .set(userObj).then(res => {
+    //             console.log("User successfully created.")
+    //         })
+    //             .catch(err => {
+    //                 console.log("Error", err)
+    //             })
+    //     }).catch(function (error) {
+    //         window.alert(error.message);
+    //     });
+    // }, []);
     return (
         <div >
             <SignUpTemplate />
