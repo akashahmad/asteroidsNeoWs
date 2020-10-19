271bfb78f5d1c6cc632e5e7befd4a3046763f1ad
akashahmad <engr.akashahmad@gmail.com>
2020-10-20 04:50:41 +0500

    onAuthStateChanged shifted to the routes

--- src/privateRoutes.tsx
+++ src/privateRoutes.tsx
@@ -1,54 +1,16 @@
-import React, {useEffect, useContext} from 'react';
-import {Route, Switch, withRouter} from 'react-router-dom';
+import React, {useContext} from 'react';
+import {Route, Switch} from 'react-router-dom';
 import {GlobalContext} from './context/GlobalState';
 import ListOfAsteroids from './pages/asteroidsList/asteroidsList'
 import Loader from './organisms/loader'
-import firebase from "./config/firebase";
 
-const Routes = (props: any) => {
-    let {history, location} = props;
-    const {setLoader, setUser, setId, setLoggedIn, loggedIn, setFavouriteIds, setFavouriteAsteroids}:any = useContext(GlobalContext);
-    useEffect(() => {
-        setLoader(true);
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                firebase.firestore().collection('users').doc(user.uid).onSnapshot((snapshot) => {
-                    let data = {...snapshot.data()};
-                    setUser(data);
-                    setId(user.uid);
-                    setLoader(false);
-                    setLoggedIn(true);
-                });
-                firebase.firestore().collection('usersFavourites').where("userId", "==", user.uid).onSnapshot((snapshot) => {
-                    if (snapshot.size) {
-                        let ids:any = [];
-                        let favourites:any = [];
-                        snapshot.forEach(each => {
-                            ids.push(each.id);
-                            favourites.push(each.data());
-                        });
-                        setFavouriteIds(ids);
-                        setFavouriteAsteroids(favourites);
-                    }
-                });
-            } else {
-                if (location.pathname === "/sign-up") {
-                }
-                else if (location.pathname === "/log-in") {
-                }
-                else {
-                    history.push("/log-in");
-                }
-            }
-        });
-        setLoader(false)
-        // eslint-disable-next-line
-    }, [location.pathname]);
+const Routes = () => {
+    const {loggedIn}:any = useContext(GlobalContext);
     return (
         <Switch>
             <Route path="/" exact component={ loggedIn ? ListOfAsteroids : Loader }/>
         </Switch>
     )
 }
-export default withRouter(Routes);
+export default Routes;
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
-import React, {useContext} from 'react'
-import {Route, Switch} from 'react-router-dom';
+import React, {useEffect, useContext} from 'react';
+import {Route, Switch, withRouter} from 'react-router-dom';
 import {NotificationContainer} from 'react-notifications';
 import SignUpPage from './pages/signUpPage/signUppage'
 import LogInPage from './pages/logInPage/logInPage'
@@ -7,9 +7,56 @@ import PrivateRoutes from './privateRoutes'
 import {GlobalContext} from './context/GlobalState';
 import CommonLoader from './organisms/loader';
 import 'react-notifications/lib/notifications.css';
+import firebase from "./config/firebase";
 
-export default () => {
+const Routes = (props: any) => {
+    let {history, location} = props;
     const {loader} = useContext(GlobalContext);
+    const {setLoader, setUser, setId, setLoggedIn, loggedIn, setFavouriteIds, setFavouriteAsteroids}:any = useContext(GlobalContext);
+    useEffect(() => {
+        if (loggedIn) {
+            if (location.pathname === "/sign-up") {
+                history.push("/");
+            } else if (location.pathname === "/log-in") {
+                history.push("/");
+            }
+        } else {
+            setLoader(true);
+            firebase.auth().onAuthStateChanged((user) => {
+                if (user) {
+                    firebase.firestore().collection('users').doc(user.uid).onSnapshot((snapshot) => {
+                        let data = {...snapshot.data()};
+                        setUser(data);
+                        setId(user.uid);
+                        setLoader(false);
+                        setLoggedIn(true);
+                    });
+                    firebase.firestore().collection('usersFavourites').where("userId", "==", user.uid).onSnapshot((snapshot) => {
+                        if (snapshot.size) {
+                            let ids: any = [];
+                            let favourites: any = [];
+                            snapshot.forEach(each => {
+                                ids.push(each.id);
+                                favourites.push(each.data());
+                            });
+                            setFavouriteIds(ids);
+                            setFavouriteAsteroids(favourites);
+                        }
+                    });
+                } else {
+                    if (location.pathname === "/sign-up") {
+                    }
+                    else if (location.pathname === "/log-in") {
+                    }
+                    else {
+                        history.push("/log-in");
+                    }
+                }
+            });
+            setLoader(false)
+        }
+        // eslint-disable-next-line
+    }, [location.pathname, loggedIn]);
     return (
         <>
         <NotificationContainer/>
@@ -18,7 +65,9 @@ export default () => {
             <Route path="/log-in" component={ LogInPage }/>
             <Route path="/" component={ PrivateRoutes }/>
         </Switch>
-        {loader?<CommonLoader/>:<></>}
+        {loader ? <CommonLoader/> : <></>}
         </>
     )
-}
+};
+
+export default withRouter(Routes);
